83f165fb2d66271eed90a19c39026d8ce170816f
Travis Nielsen <trn@nielski.com>
2021-04-08 14:22:21 -0500

    base api

---
+++ services/SessionRegulator/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from express and typescript');
+});
+
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
